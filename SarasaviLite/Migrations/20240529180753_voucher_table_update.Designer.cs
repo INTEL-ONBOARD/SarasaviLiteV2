93d8914c253b0ed55e469cfd0d339e70b32c2dca
VIHANGA <vihanga2003nimsara@gmail.com>
2024-05-29 23:38:17 +0530

    added some columns to voucher table

--- SarasaviLite/Migrations/20240529180753_voucher_table_update.Designer.cs
+++ SarasaviLite/Migrations/20240529180753_voucher_table_update.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SarasaviLite.Data;
 
@@ -11,9 +12,11 @@ using SarasaviLite.Data;
 namespace SarasaviLite.Migrations
 {
     [DbContext(typeof(SarasaviContext))]
-    partial class SarasaviContextModelSnapshot : ModelSnapshot
+    [Migration("20240529180753_voucher_table_update")]
+    partial class voucher_table_update
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SarasaviLite/Migrations/20240529180753_voucher_table_update.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SarasaviLite.Migrations
+{
+    /// <inheritdoc />
+    public partial class voucher_table_update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Qty",
+                table: "Vouchers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Value",
+                table: "Vouchers",
+                type: "decimal(10,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Qty",
+                table: "Vouchers");
+
+            migrationBuilder.DropColumn(
+                name: "Value",
+                table: "Vouchers");
+        }
+    }
+}
